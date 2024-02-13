3c991ef3381bb10f14672d08ad05aca5a99d40af
Breno <brenorusciolelli@gmail.com>
2024-02-13 14:55:30 -0300

    initial commit

---
+++ bota-na-rifa/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+      <img src="../assets/img/logo.png" alt="">
+    </header>
+  </template>
+  
+  <style>
+    header {
+    position: fixed;
+    top: 10;
+    left: 0;
+    right: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    }
+  </style>
+  
---
+++ bota-na-rifa/src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/inicio',
+      name: 'page-inicio',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HomeView.vue')
+    }
+  ]
+})
+
+export default router
